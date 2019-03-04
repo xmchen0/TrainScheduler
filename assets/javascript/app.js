751ee66a8667344686d06a26ac99f03a57a5cd36
Kathy Chen <kathychen.social@gmail.com>
2019-03-04 00:14:32 -0500

    added changes to logic file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,19 @@
+/* 
+
+UofT Boot Camp -- Train Scheduler
+
+Credit references to week 07 - activity 19 Add_Child
+
+*/
+
+
 $(document).ready(function () {
     // Current Date and Time
     var currentTime = new Date()
     var hours = currentTime.getHours()
     var minutes = currentTime.getMinutes()
-    var seconds = currentTime.getSeconds()
 
     $('#currentDateTime').html("<b>" + hours + ":" + minutes + " " + "</b>");
-    $('#currentDateTime').datepicker({ dateFormat: "yy/mm/dd" }).datepicker("setDate", new Date());
 
     // Initialize Firebase
     var config = {
@@ -19,6 +26,42 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 
-   
+    // Get database
+    var database = firebase.database();
+
+    // Initial Values
+    var name;
+    var destination;
+    var firstTrain;
+    var frequency = 0;
+
+    // Capture Button Click
+    $("#submitBtn").on("click", function () {
+        event.preventDefault();
+
+        // Storing and retreiving most recent train data
+        name = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstTrain = $("#firstTrainTimeInput").val().trim();
+        frequency = $("#frequencyInput").val().trim();
+
+        // Code for the push to database
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        // Firebase watcher + initial loader
+        database.ref().on("child_added", function (childSnapshot) {
+     
+
+            // Handle the errors
+        }, function (errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        });
+    });
 
 });
